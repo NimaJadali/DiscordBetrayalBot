4d7b1eea4331a10894cc2fdd26cd0116090ad114
Nima Jadali <nimajadalipour@gmail.com>
2021-08-19 01:41:14 -0400

    Update main.py
    Fixed the visualization

--- main.py
+++ main.py
@@ -7,18 +7,35 @@ columns = 36
 
 client = discord.Client()
 
+
+def add(coords, direction):
+  return coords[0] + direction[0], coords[1] + direction[1]
+
+
+def visualize(temp):
+  currMap = ""
+  for i in range(rows):
+    for j in range(columns):
+      if (i == temp[0] and j == temp[1]):
+        currMap += "0"
+      else:
+        currMap += "┼"
+    currMap += "\n"
+  return currMap
+
+
 @client.event
 async def on_ready():
   print("I'm ready. Logged in as {0.user}".format(client))
   db.clear()
   db["game_values"] = {"created": False, "wantingToEndGame": False, "usersWantingToEndgame": []}
 
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
-  
-  #row = "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼"
+
   if message.content == "/help":
     msg = "This is a game test. For rules run /rules. To create a game run /createGame. To join the game type /join. To add an icon type /icon <emoji>. To start the game type /start."
     await message.channel.send(msg)
@@ -87,7 +104,7 @@ async def on_message(message):
       for user in db["users"].value:
         db[user]["location"] = (8, 18)
       # Visualize
-      visualize()
+      msg += "\n" + visualize(db[user]["location"])
       # Start First Round
     await message.channel.send(msg)
 
@@ -122,7 +139,6 @@ async def on_message(message):
       msg_list = list(message.content.split(" "))
       if len(msg_list) != 3:
         msg = "bad command. Need two argument /move <direction> <distance>. Directions are left, right, up, and down."
-      print(db[str(message.author)]["location"])
       # Update Player Location
       if msg_list[1] == "left":
         direction = (0, -int(msg_list[2]))
@@ -133,25 +149,10 @@ async def on_message(message):
       elif msg_list[1] == "down":
         direction = (int(msg_list[2]), 0)
       db[str(message.author)]["location"] = add(db[str(message.author)]["location"], direction)
-      print(db[str(message.author)]["location"])
       # Visualize
-      msg = visualize(db[str(message.author)]["location"])
+      msg = visualize(db[str(message.author)]["location"].value)
       # 
     await message.channel.send(msg)
 
 bot_token = os.environ['bot_token']
 client.run(bot_token)
-
-def add(coords, direction):
-  return coords[0] + direction[0], coords[1] + direction[1]
-
-def visualize(temp):
-  currMap = ""
-  for i in range(rows):
-    for j in range(columns):
-      if (i,j) == temp:
-        "0"
-      else:
-        currMap += "┼"
-    currMap += "\n"
-  return currMap
